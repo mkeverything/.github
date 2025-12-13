5d15d0864875a72d89a02e3cddae7f76c9bc1463
Matvey Sokolovsky <matvey10@gmail.com>
2025-12-12 18:55:37 -0700

    Update GitHub link in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const App = () => (
 			mk means make<span className='animate-logo-pulse'>_</span>
 		</p>
 		<Link
-			href='https://github.com/mkcode-org'
+			href='https://github.com/mkeverything'
 			className='text-2xl absolute bottom-16 underline underline-offset-2 hover:opacity-50'
 		>
 			github
